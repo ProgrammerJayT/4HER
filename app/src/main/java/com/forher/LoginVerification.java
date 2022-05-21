32b83e9dadbf77f5da6f08250aad1960c29e4259
ProgrammerJayT <theanthem8@gmail.com>
2022-05-21 21:22:54 +0200

    Configured sign in with email and password

--- app/src/main/java/com/forher/LoginVerification.java
+++ app/src/main/java/com/forher/LoginVerification.java
@@ -1,25 +1,47 @@
 package com.forher;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginVerification extends AppCompatActivity {
 
-    LoginCredentials credentials = new LoginCredentials();
-    FirebaseDatabase database;
-    DatabaseReference testRef;
+    private final LoginCredentials credentials = new LoginCredentials();
+    private FirebaseDatabase database;
+    private DatabaseReference testRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_verification);
 
-        Toast.makeText(this, "Please wait while we verify your credentials", Toast.LENGTH_LONG).show();
+        String email = credentials.getEmail();
+        String password = credentials.getPassword();
 
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()){
+                Toast.makeText(this, "Welcome back", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginVerification.this, Home.class));
+            }
+            else {
+                Toast.makeText(this, "Couldn't verify credentials", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginVerification.this, Login.class));
+            }
+            finish();
+        });
     }
 }
